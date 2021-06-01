574ecff891ce4f5e579159926ab08e0e142cb14c
Dakota <dakota.c.upchurch@gmail.com>
2021-06-01 10:37:51 -0500

    phase 2 complete

--- Member.cs
+++ Member.cs
@@ -2,12 +2,20 @@ using System;
 
 namespace Heist
 {
-    class Member
+    public class Member
     {
         public string Name { get; set; }
         public int SkillLevel { get; set; }
         public double Courage { get; set; }
 
 
+        public Member(string name, int skillLevel, double courage)
+        {
+            Name = name;
+            SkillLevel = skillLevel;
+            Courage = courage;
+        }
     }
+
+
 }
